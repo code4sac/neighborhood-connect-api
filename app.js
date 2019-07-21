5e02af94d88481d2dfaa9042be6d258bfe0ac1d4
wizard-jS <spraguej624@gmail.com>
2019-07-21 10:16:26 -0700

    Add userModel

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-const app = require('express')();
-const priorities = require('./server/controllers/priorityRouter.js');
-const organizations = require('./server/controllers/orgRouter.js');
-const users = require('./server/controllers/userRouter.js');
-const types = require('./server/controllers/typeRouter.js');
+const app = require("express")();
+const priorities = require("./server/controllers/priorityRouter.js");
+const organizations = require("./server/controllers/orgRouter.js");
+const users = require("./server/controllers/userRouter.js");
+const types = require("./server/controllers/typeRouter.js");
 
-
-app.use('/priorities', priorities);
-app.use('/orgs', organizations);
-app.use('/users', users);
-app.use('/types', types);
+app.use("/priorities", priorities);
+app.use("/orgs", organizations);
+app.use("/users", users);
+app.use("/types", types);
 
 app.listen(3000);
