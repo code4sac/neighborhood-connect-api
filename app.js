9484e92817dfeb9dfd4922566685e80b708e399c
Michael Landers <landers.mike@gmail.com>
2019-07-21 13:05:58 -0700

    added events into app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const organizations = require('./server/controllers/orgRouter.js');
 // const users = require("./server/controllers/userRouter.js");
 const types = require('./server/controllers/typeRouter.js');
+const events = require('./server/controllers/eventsRouter.js');
 
 // Middleware
 app.use(morgan('dev'));
@@ -15,6 +16,7 @@ app.use(morgan('dev'));
 app.use('/orgs', organizations);
 // app.use("/users", users);
 app.use('/types', types);
+app.use('/events', events);
 
 // app.use(express.static(path.join(__dirname, './build')));
 
--- server/model/db.js
+++ server/model/db.js
@@ -2,8 +2,6 @@ const { Client } = require('pg');
 const config = require('./config.js');
 const client = new Client(config);
 client.query(`set search_path to 'test'`);
-client.connect(() => {
-	console.log('Hido ho, Captn! We are connected');
-});
+client.connect();
 
 module.exports = client;
