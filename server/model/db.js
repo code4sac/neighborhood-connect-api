d33fdb929024a43c84ebdcefcafb91424283e328
Pablo Frias <pafrias90@gmail.com>
2019-07-21 12:05:16 -0700

    Add searchpath to db config to automatically use text db

--- server/model/db.js
+++ server/model/db.js
@@ -2,5 +2,6 @@ const { Client } = require('pg');
 const config = require('./config.js');
 const client = new Client(config);
 client.connect();
+client.query(`set search_path to 'test'`)
 
 module.exports = client;
