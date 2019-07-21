957cc140be8543ffae717998df1d6c9f1ee2364f
Michael Landers <landers.mike@gmail.com>
2019-07-21 15:03:38 -0700

    adjusted getEventByType response

--- server/model/Events.js
+++ server/model/Events.js
@@ -12,7 +12,11 @@ module.exports = {
 	},
 
 	getEventByType(id, cb) {
-		let query = `select * from action inner join action_type on action.action_type_id=action_type.id where action_type.id = ${id};`;
+		// let query = `select * from action inner join action_type on action.action_type_id=action_type.id where action_type.id = ${id};`;
+		let query = `select a.id, t.name as action_type, a.title, a.description, a.timestamp, a.visibility, p.name as priority_status from action  a
+		INNER JOIN action_type t on a.action_type_id=t.id
+		INNER JOIN priority_status_type p on p.id = a.priority_id
+		WHERE t.id = ${id}`;
 		db.query(query, cb);
 	}
 };
