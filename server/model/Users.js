4de3cf2b62e2040bd7a0e0b4bbce0c0a2c9bf3cf
wizard-jS <spraguej624@gmail.com>
2019-07-20 20:58:37 -0700

    Add initial user models

---
+++ server/model/Users.js
@@ -0,0 +1,41 @@
+const db = require("../model/db");
+
+module.exports = {
+  readUser: async id => {
+    try {
+      const result = await db.query(
+        `select * from test.users where id = ${id}`
+      );
+      db.end();
+    } catch (err) {
+      return err;
+    }
+  },
+  readAllUsers: async () => {
+    try {
+      const result = await db.query(`select * from test.organization`);
+      return result;
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+  },
+  createUser: async params => {
+    //todo params
+    try {
+      const result = await db.query("insert...");
+      return result;
+    } catch (err) {
+      return err;
+    }
+  },
+  updateUser: async params => {
+    // todo params
+    try {
+      const result = await db.query("update");
+      return result;
+    } catch (err) {
+      return err;
+    }
+  }
+};
