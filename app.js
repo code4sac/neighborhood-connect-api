bd0fe4c9546574e490c3c4f5dccdc8a4143f11cb
Ryan <ryandoyle@me.com>
2019-07-23 12:43:13 -0700

    add body-parser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const bodyParser = require('body-parser')
 
 const {
 	priorityRouter,
@@ -18,6 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
+app.use(bodyParser.json());
 
 // Routes
 app.use('/orgs', organizations);
