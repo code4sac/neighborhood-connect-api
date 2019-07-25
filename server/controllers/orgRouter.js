d32cb4cf64220564631bbdac6fbd752b3a30977e
Pablo Frias <pafrias90@gmail.com>
2019-07-24 18:44:18 -0700

    Implement ESlint and linting npm scripts. Code clean up
    Linting has been configured to Google's Node style guide. Priorities
    router has been implemented more in line with the other modules.

--- server/controllers/orgRouter.js
+++ server/controllers/orgRouter.js
@@ -1,34 +1,36 @@
 const express = require('express');
+// eslint-disable-next-line new-cap
 const router = express.Router();
-const { getOrg, getOrgUsers, postOrg } = require("../model/Organizations");
+const {getOrg, getOrgUsers, postOrg} = require('../model/Organizations');
 
 // DRY consider refactoring
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    res.status(200).send(await getOrg(null))
+    const result = await getOrg(null);
+    res.status(200).send(result.rows);
   } catch (err) {
     res.status(404).send(err);
   }
 });
 
-router.get("/:orgId", async (req, res) => {
+router.get('/:orgId', async (req, res) => {
   try {
-    res.status(200).send(await getOrg(req.params.orgId))
+    res.status(200).send(await getOrg(req.params.orgId));
   } catch (err) {
     console.log(err);
     res.status(404).send(err);
   }
 });
 
-router.get("/:orgId/users", async (req, res) => {
+router.get('/:orgId/users', async (req, res) => {
   try {
-    res.status(200).send(await getOrgUsers())
+    res.status(200).send(await getOrgUsers());
   } catch (err) {
     res.status(404).send(err);
   }
 });
 
-router.post('/', express.json(), async(req, res) => {
+router.post('/', async (req, res) => {
   try {
     await postOrg(req.body);
     res.sendStatus(200);
@@ -36,11 +38,11 @@ router.post('/', express.json(), async(req, res) => {
     console.log(err);
     res.status(404).send(err);
   }
-})
+});
 
-router.get("/:orgId/users/:userId", async (req, res) => {
+router.get('/:orgId/users/:userId', async (req, res) => {
   try {
-    res.status(200).send(await getOrgUsers(req.params.orgId))
+    res.status(200).send(await getOrgUsers(req.params.orgId));
   } catch (err) {
     res.status(404).send(err);
   }
