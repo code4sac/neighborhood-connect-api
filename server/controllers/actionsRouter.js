f1b25ea030a1f074fe688a47b1e782cf820027a2
Ryan <ryandoyle@me.com>
2019-07-26 13:48:33 -0700

    make post to / async to send correct status to client

--- server/controllers/actionsRouter.js
+++ server/controllers/actionsRouter.js
@@ -17,10 +17,10 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  const result = createAction(req.body);
+router.post('/', async (req, res) => {
+  const result = await createAction(req.body);
   if (result === 1) {
-    res.status(201).send(result);
+    res.sendStatus(201).send(result);
   } else {
     res.status(500).send(result);
   }
