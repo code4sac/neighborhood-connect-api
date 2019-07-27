bf1dad138ca84cb8d65233a66d14159214dd9e2e
Jonathan Throne <jthrone@skyslope.com>
2019-07-27 14:04:02 -0700

    Part 2 forgot to add these untracked files.

---
+++ server/services/notificationService.js
@@ -0,0 +1,59 @@
+const AWS = require('aws-sdk');
+const Logger = require('logService.js');
+
+const SourceEmail = 'neighborhoodconnect.sac@gmail.com';
+
+AWS.config.loadFromPath('configs/aws.config.json');
+
+const ses = new AWS.SES();
+const sns = new AWS.SNS();
+
+const NotificationService = {
+  sendEmail: async (toEmail, subjectText, bodyText) => {
+    try {
+      const params = {
+        Destination: {
+          ToAddresses: [
+            toEmail,
+          ],
+        },
+        Message: {
+          Body: {
+            Text: {
+              Charset: 'UTF-8',
+              Data: bodyText,
+            },
+          },
+          Subject: {
+            Charset: 'UTF-8',
+            Data: subjectText,
+          },
+        },
+        Source: SourceEmail,
+      };
+
+      const data = await ses.sendEmail(params).promise();
+
+      Logger.logDebug(data.MessageId);
+    } catch (err) {
+      Logger.logError(err);
+    }
+  },
+  sendSMS: async (phoneNumber, message) => {
+    try {
+      const params = {
+        Message: message,
+        PhoneNumber: phoneNumber,
+      };
+
+      // Create the promise and SES service object
+      const data = await sns.publish(params).promise();
+
+      Logger.logDebug(data.MessageId);
+    } catch (err) {
+      Logger.logError(err);
+    }
+  },
+};
+
+module.exports.NotificationService = NotificationService;
---
+++ server/utils.js
@@ -0,0 +1,5 @@
+const Utilities = {
+  isProdEnv: () => (process.env.NODE_ENV === 'production'),
+};
+
+module.exports.Utilities = Utilities;
